326e11a7485533b048db7459f0dd52d69e681095
Olivier Laviale <contact@weirdog.com>
2010-04-25 22:28:41 +0200

    Rewrote class WdHook

--- wdhook.php
+++ wdhook.php
@@ -11,10 +11,6 @@
 
 class WdHook
 {
-	const T_CALLBACK = 0;
-	const T_PARAMS = 1;
-	const T_PARENT = 'parent';
-
 	static protected $namespaces = array();
 	static protected $hooks = array();
 
@@ -116,155 +112,35 @@ class WdHook
 		return self::$hooks[$ns][$name];
 	}
 
-	/*
-	 * object
-	 */
-
+	public $callback;
+	public $params = array();
 	public $tags = array();
-	private $definitions = array();
 
-	public function __construct(array $tags)
+	public function __construct($callback, array $params=array(), array $tags=array())
 	{
+		$this->callback = $callback;
+		$this->params = $params;
 		$this->tags = $tags;
-
-		foreach ($tags as $tag => $value)
-		{
-			switch ($tag)
-			{
-				case self::T_CALLBACK:
-				{
-					$this->callback = $value;
-				}
-				break;
-
-				case self::T_PARENT:
-				{
-					$this->parent = $value;
-				}
-				break;
-
-				case self::T_PARAMS:
-				{
-					foreach ($value as $name => $definition)
-					{
-						if (!is_array($definition))
-						{
-							$definition = array('default' => $definition);
-						}
-
-						if (!array_key_exists('default', $definition))
-						{
-							$definition['default'] = null;
-						}
-
-						foreach ($definition as $key => $value)
-						{
-							$this->definitions[$key][$name] = $value;
-						}
-					}
-				}
-				break;
-			}
-		}
-
-//		echo l('<h3>hook</h3>\1', $this);
 	}
 
-	public function call(array $params=array())
+	public $args = array();
+
+	public function call(array $args=array())
 	{
-		$args = func_get_args();
+		$user_args = func_get_args();
 
-		array_shift($args);
-		/*
-		foreach ($params as $i => $value)
-		{
-			if (!is_numeric($i))
-			{
-				continue;
-			}
-
-			$args[$i] = $value;
-
-			unset($params[$i]);
-		}
-
-		$this->params = $this->checkParams($params);
-		*/
+		array_shift($user_args);
 
 		$clone = clone $this;
 
-		$clone->params = $params;
+		$clone->args = $args;
 
-		array_unshift($args, $clone);
+		array_unshift($user_args, $clone);
 
-		$rc = call_user_func_array($clone->callback, $args);
+		$rc = call_user_func_array($clone->callback, $user_args);
 
 		unset($clone);
 
 		return $rc;
-	}
-
-	public function checkParams($params)
-	{
-		if (empty($this->definitions['default']))
-		{
-			return $params;
-		}
-
-		$defaults = $this->definitions['default'];
-
-		#
-		# check for extraneous parameters
-		#
-
-		foreach ($params as $name => $value)
-		{
-			if (!array_key_exists($name, $defaults))
-			{
-				WdDebug::trigger
-				(
-					'Extraneous parameter %name with value: !value', array
-					(
-						'%name' => $name,
-						'!value' => $value
-					)
-				);
-
-				continue;
-			}
-		}
-
-		#
-		# check mandatory attributes
-		#
-
-		if (isset($this->definitions['mandatory']))
-		{
-			foreach ($this->definitions['mandatory'] as $name => $value)
-			{
-				if (!$value)
-				{
-					continue;
-				}
-
-				if (!array_key_exists($name, $params))
-				{
-					WdDebug::trigger
-					(
-						'The %name parameter is mandatory', array
-						(
-							'%name' => $name
-						)
-					);
-				}
-			}
-		}
-
-		return $params + $defaults;
-	}
-
-	public function getDefinition($definition)
-	{
-		return empty($this->definitions[$definition]) ? array() : $this->definitions[$definition];
 	}
 }
